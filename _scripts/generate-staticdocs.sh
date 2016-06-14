f3ba446c52936d7fa5bc0323a71bcdddddacc4f9
JJ Allaire <jj@rstudio.org>
2016-06-14 16:43:34 -0400

    more work on staticdocs

--- _scripts/generate-staticdocs.sh
+++ _scripts/generate-staticdocs.sh
@@ -7,6 +7,8 @@ if [[ `basename $PWD` != "spark.rstudio.com" ]]; then
    exit 1
 fi
 
+rm -rf reference/sparklyr/latest/
+mkdir -p reference/sparklyr/latest/
 cp ~/packages/rspark/inst/staticdocs/index.r reference/sparklyr/latest/index.R
 R --slave -e "set.seed(1337);library(staticdocs);library(htmltools);build_site('~/packages/rspark', site_path='reference/sparklyr/latest', templates_path='_staticdocs', examples=FALSE);warnings()"
 rm -rf reference/sparklyr/latest/index.R

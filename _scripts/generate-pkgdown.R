af5dbec9156a7e0c75e03a0c5fc664a50280531a
Edgar Ruiz <edgar@ip-10-84-142-94.us-west-2.compute.internal>
2016-12-14 19:13:34 +0000

    Pkgdown update part deux

---
+++ _scripts/generate-pkgdown.R
@@ -0,0 +1,25 @@
+devtools::install_github("edgararuiz/pkgdown")
+library(pkgdown)
+library(sparklyr)
+
+#----- Initializing main path variables
+root <- rprojroot::find_rstudio_root_file()
+sparklyr_root <- file.path(root, "sparklyr_github")
+original_wd <- getwd()
+
+#------ Downloading a fresh copy of the code from github
+unlink(sparklyr_root, recursive = TRUE)
+system(paste("git clone https://github.com/rstudio/sparklyr -b master", sparklyr_root))
+
+#------ Replacing pkgdown's default yml file with the site's & removing the index.html created by Build Website
+file.copy(file.path(root,"_site.yml"), file.path(sparklyr_root,  "_pkgdown.yml"), overwrite = TRUE)
+file.remove(file.path(root,"_site/index.html"))
+
+#------- Creating the site
+setwd(sparklyr_root)
+if(file.exists("README.Rmd"))file.remove("README.Rmd")
+system(paste("cp -R ", file.path(sparklyr_root, "README_files"), file.path(root, "_site/")))
+build_site(path=file.path(root,"_site"))
+setwd(original_wd)
+
+
--- _scripts/generate-staticdocs.R
+++ _scripts/generate-staticdocs.R
@@ -11,21 +11,23 @@ ensure_package <- function(source) {
   stop("failed to install / load package '", package, "'")
 }
 
-ensure_package("hadley/staticdocs")
+ensure_package("edgararuiz/pkgdown")
 ensure_package("rstudio/sparklyr")
 ensure_package("rstudio/htmltools")
 ensure_package("krlmlr/rprojroot")
+library(pkgdown)
 
 root <- rprojroot::find_rstudio_root_file()
 if (!identical(basename(root), "spark.rstudio.com"))
   stop("script was not run from 'spark.rstudio.com' directory", call. = FALSE)
 
 # ensure sparklyr package sources
-info <- file.info(file.path(root, ".sparklyr"))
-if (!isTRUE(info$isdir))
-  system(paste("git clone https://github.com/rstudio/sparklyr -b master", file.path(root, ".sparklyr")))
+info <- file.info(file.path(root, "sparklyr_github"))
+if (isTRUE(info$isdir))unlink(file.path(root, "sparklyr_github"), recursive = TRUE)
+system(paste("git clone https://github.com/rstudio/sparklyr -b master", file.path(root, "sparklyr_github")))
+file.copy(file.path(root,"_site.yml"),file.path(root, "sparklyr_github","_site.yml") )
 
-owd <- setwd(file.path(root, ".sparklyr"))
+owd <- setwd(file.path(root, "sparklyr_github"))
 system("git fetch && git checkout master")
 setwd(owd)
 
@@ -35,12 +37,15 @@ unlink(ref_dir, recursive = TRUE)
 dir.create(ref_dir, recursive = TRUE)
 
 # generate staticdocs page
-staticdocs::build_site(
-  pkg = file.path(root, ".sparklyr"),
-  site_path = ref_dir,
-  templates_path = file.path(root, "_staticdocs"),
+
+template_navbar( file.path(root, "sparklyr_github","_site.yml") )
+build_reference(
+  pkg = file.path(root, "sparklyr_github"),
+  path = ref_dir,
+  #templates_path = file.path(root, "_staticdocs"),
+  #run_dont_run = TRUE,
   examples = FALSE,
-  launch = FALSE,
+  #launch = FALSE,
   seed = 1337
 )
 
@@ -55,3 +60,8 @@ if (!copied)
   stop("failed to populate 'index.html'", call. = FALSE)
 
 message(">>> staticdocs created successfully.")
+
+
+
+
+

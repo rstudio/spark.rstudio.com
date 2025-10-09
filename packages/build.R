569c22be9d1d366c4383dae36b1728921b2f26fa
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-10-09 16:47:14 -0500

    Updates pysparklyr

--- packages/build.R
+++ packages/build.R
@@ -1,17 +1,31 @@
 library(pkgsite)
 library(gert)
+library(glue)
 library(fs)
 
-dir_delete("_packages/sparkxgb/")
-git_clone("https://github.com/rstudio/sparkxgb", path = "_packages/sparkxgb/")
-
-
-write_reference(
-  project = "_packages/sparkxgb/", 
-  folder = "packages/sparkxgb/reference", 
-  examples = FALSE
+build_reference <- function(pkg, org, sub = NULL) {
+  clone <- path("_packages", pkg)
+  if(is.null(sub)) {
+    target <- path("packages", pkg)
+  } else {
+    target <- path("packages", pkg, sub)
+  }
+  repo_url <- glue("https://github.com/{org}/{pkg}")
+  if(dir_exists(clone)) {
+    system2("git", paste0("-C '", path_abs(clone), "' pull ", repo_url, " -q"))
+  } else {
+    git_clone(repo_url, path = clone)  
+  }
+  write_reference(
+    project = clone, 
+    folder = path(target, "reference"), 
+    examples = FALSE
   )
+  file_copy(path(clone, "NEWS.md"), path(target, "news.md"), overwrite = TRUE)  
+}
 
+build_reference("sparklyr", "sparklyr", "latest")
+build_reference("pysparklyr", "mlverse")
 
 
 
